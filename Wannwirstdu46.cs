3d295b31f624991971ae5b3aa0209069f2ce17db
IvanKos19 <142885533+IvanKos19@users.noreply.github.com>
2023-09-26 11:20:30 +0200

    Add files via upload

---
+++ Wannwirstdu46.cs
@@ -0,0 +1,15 @@
+﻿namespace WannwirstduAlt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie alt sind sie?: ");
+            int Alter = Convert.ToInt32(Console.ReadLine());
+
+            double Summe = 46 - Alter;
+            double Altesalter = Summe + Alter;
+            Console.WriteLine("In " + Summe + " werden sie " + Altesalter + " alt:");
+        }
+    }
+}
