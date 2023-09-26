be0acfb198bdb53e1ddaa15dad19431ba5e179aa
IvanKos19 <142885533+IvanKos19@users.noreply.github.com>
2023-09-26 11:22:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Notenrechner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Willkommen beim Notenrechner");
+            double BenutzerNote = Noteneingabe();
+
+
+
+
+
+        }
+        static double Noteneingabe()
+        {
+            double Note = 0;
+            Boolean isCorrectInput = false;
+            while (!isCorrectInput)
+            {
+                try
+                {
+                    Console.WriteLine("Geben sie ihre Note ein: ");
+                    Note = Convert.ToDouble(Console.ReadLine());
+
+                    if (Note < 1 || Note > 6)
+                    {
+                        throw new Exception();
+                    } else
+                    {
+                        isCorrectInput = true;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ungültige Eingabe");
+                }
+            }
+            return Note;
+
+        }
+    }
+}
+
